3ea15edc999c29273b1ea51082eaea7730b41b99
DonDhaky <erick.thonon@epitech.eu>
2024-04-23 11:34:55 +0200

    [EDIT] index.js

--- back/routes/index.js
+++ back/routes/index.js
@@ -2,6 +2,33 @@ var express = require('express');
 var router = express.Router();
 var database = require('../database');
 
+///////////////////////////////////////////////////////////////////// API
+require('dotenv').config();
+var scope = 'user-read-private user-read-email';
+const spotifyWebApi = require('spotify-web-api-node');
+const spotifyApi = new spotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUrl: process.env.REDIRECT_URL
+});
+
+
+///////////////////////////////////////////////////////////////////// API
+app.get('/login', function(req, res) {
+
+    var state = generateRandomString(16);
+    var scope = 'user-read-private user-read-email';
+  
+    res.redirect('https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+      }));
+  });
+
 ///////////////////////////////////////////////////////////////////// GO TO HOME PAGE
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', session : req.session });
