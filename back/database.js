f6d550a079a0eee2a86837e7bcffdd04bb60be87
DonDhaky <erick.thonon@epitech.eu>
2024-06-19 18:21:12 +0200

    resolving bug repo vide

--- back/database.js
+++ back/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
 	host : 'localhost',
 	database : 'login',
-	user : 'clem',
-	password : '1234'
+	user : 'erickk',
+	password : '744466'
 });
 
 connection.connect(function(error){
